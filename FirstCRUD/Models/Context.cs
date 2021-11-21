5cdaab0469ce057cc940e3af14c4d8ca7db772a4
vitoria-isabela <vitoriaisabela368@gmail.com>
2021-11-21 19:46:19 -0300

    Enviando etapa básica do projeto

---
+++ FirstCRUD/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCRUD.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Carro> Carros { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> option) : base(option)
+        {
+
+        }
+    }
+}
---
+++ FirstCRUD/Startup.cs
@@ -0,0 +1,72 @@
+﻿using FirstCRUD.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCRUD
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<Contexto>(option => option.UseSqlServer(Configuration.GetConnectionString("ConexaoDB")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                //multiple routes
+                //routes.MapRoute(
+                //    "teste-vitoria",
+                //    "sobre/sobrenos",
+                //    defaults: new { Controller = "Home", Action = "About" });
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Carros}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
